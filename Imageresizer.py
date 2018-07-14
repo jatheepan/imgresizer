6b698ea07dbcfe43a6ea14ed4d05a7173dbb42e6
Theepan Kanthavel <theepan@cutedrops.com>
2018-07-14 07:53:13 -0400

    created and used widget for choosing folder

--- Imageresizer.py
+++ Imageresizer.py
@@ -1,9 +1,8 @@
 from Tkinter import *
 import ttk, tkFileDialog
+from gui.ChooseFolder import ChooseFolder
 
-# print(dir(ttk.Style.configure))
 master = Tk()
-
 
 def browse(target_input):
   filename = tkFileDialog.askdirectory()
@@ -14,26 +13,21 @@ def browse(target_input):
 main_wrapper = ttk.Frame(master)
 main_container = ttk.Frame(main_wrapper)
 title = ttk.Label(main_container, text='Image Resizer')
-input_label = ttk.Label(main_container, text='Input Directory')
-input_entry = ttk.Entry(main_container)
-input_button = ttk.Button(main_container, text='Browse', command=lambda:browse(input_entry))
-
-output_label = ttk.Label(main_container, text='Output Directory')
-output_entry = ttk.Entry(main_container)
-output_button = ttk.Button(main_container, text='Browse', command=lambda:browse(output_entry))
-
+input_folder = ChooseFolder(main_container, **{'label_text':'Input Directory', 'label_width':15})
+output_folder = ChooseFolder(main_container, **{'label_text':'Output Directory', 'label_width':15})
 separator = ttk.Separator(main_container)
 
-resize_button = ttk.Button(main_container, text='Resize')
+
+def resize_action():
+  print(input_folder.get_value())
+
+
+resize_button = ttk.Button(main_container, text='Resize', command=resize_action)
 
 title.grid(row=0, column=0, columnspan=3, ipady=10)
-input_label.grid(row=1, column=0, sticky=W)
-input_entry.grid(row=1, column=1)
-input_button.grid(row=1, column=2)
 
-output_label.grid(row=2, column=0, sticky=W)
-output_entry.grid(row=2, column=1)
-output_button.grid(row=2, column=2)
+input_folder.grid(row=1, columnspan=3, sticky=W)
+output_folder.grid(row=2, columnspan=3, sticky=W)
 
 separator.grid(row=3, columnspan=3, sticky='news', ipady=20)
 resize_button.grid(row=4, columnspan=3)
